86889904c98ee67a7067d028396b40941aad0271
jonathansperrer <53347582+jonathansperrer@users.noreply.github.com>
2021-07-01 11:50:47 +0200

    First Commit

---
+++ UniversalServer/Model/DBAccess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalServer.Model
+{
+    public delegate EventHandler ErrorEventHandler(string msg);
+    public class DBAccess
+    {
+        public event ErrorEventHandler ErrorMessage;
+       
+        public void InsertData(TempValue t, HumidValue h, PressureValue p, DateTime dt, string ipa)
+        {
+           
+            
+        }
+    }
+}
