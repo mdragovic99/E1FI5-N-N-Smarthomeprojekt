9bd773d60e6415c75efe817a7e1bbcd729c1ef93
Marko Dragovic <marko.dragovic@AD.ITS-STUTTGART.LOGODIDACT.NET>
2021-10-08 10:37:03 +0200

    First Commit

---
+++ UniversalClient/ViewModels/MainViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using UniversalClient.ViewModelBase;
+
+namespace UniversalClient.ViewModels
+{
+    class MainViewModel:ViewModel
+    {
+        private ICommand _sendMessageCommand;
+        private static string _status;
+        
+        private static Socket _clientSocket = new Socket(AddressFamily.InterNetwork,
+            SocketType.Stream, ProtocolType.Tcp);
+        private static string _ipAddress;
+        public string IPAddressServer
+        {
+            get
+            {
+                return _ipAddress;
+            }
+            set
+            {
+                _ipAddress = value;
+                OnPropertyChanged("IPAddress");
+            }
+        }
+
+        public ICommand SendMessageCommand
+        {
+            get
+            {
+                if (_sendMessageCommand == null)
+                {
+                    _sendMessageCommand = new RelayCommand(c => ExecuteSendMessageCommand());
+                }
+                return _sendMessageCommand;
+            }
+        }
+
+        private static void ExecuteSendMessageCommand()
+        {
+            try
+            {
+                _status = "Sending Message...";
+                _clientSocket.Connect(IPAddress.Parse(_ipAddress), 666); 
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            _status = "Connected!";
+
+            
+        }
+    }
+}
---
+++ UniversalServer/Model/DBAccess.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace UniversalServer.Model
+{
+    public delegate EventHandler ErrorEventHandler(string msg);
+    public class DBAccess
+    {
+        public event ErrorEventHandler ErrorMessage;
+
+        public void Database()
+            //postgres://vkizatgz:NBribTWPJUXRh05uZE3WZkmDhHF-K4Um@balarama.db.elephantsql.com/vkizatgz
+        {
+            NpgsqlConnection conn = new NpgsqlConnection("Server=Balarama.db.elephantsql.com; Username=vkizatgz;Password=NBribTWPJUXRh05uZE3WZkmDhHF-K4Um; Database=vkizatgz");
+            conn.Open();
+        }
+       
+        public void InsertData(TempValue t, HumidValue h, PressureValue p, DateTime dt, string ipa)
+        {
+           
+            
+        }
+    }
+}
