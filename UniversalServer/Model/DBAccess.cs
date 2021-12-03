fe9ba9bf7acd56a5d8231a7c1aab629e8760c20a
jonathansperrer <jonathan@sperrer.org>
2021-12-03 23:57:35 +0100

    added insertData to git project

--- UniversalServer/Model/DBAccess.cs
+++ UniversalServer/Model/DBAccess.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using Npgsql;
 
 namespace UniversalServer.Model
@@ -11,19 +12,31 @@ namespace UniversalServer.Model
     public delegate EventHandler ErrorEventHandler(string msg);
     public class DBAccess
     {
+
+        NpgsqlConnection conn = new NpgsqlConnection("Server=Balarama.db.elephantsql.com; Username=vkizatgz;Password=NBribTWPJUXRh05uZE3WZkmDhHF-K4Um; Database=vkizatgz");
+
+
         public event ErrorEventHandler ErrorMessage;
 
+
         public void Database()
-            //postgres://vkizatgz:NBribTWPJUXRh05uZE3WZkmDhHF-K4Um@balarama.db.elephantsql.com/vkizatgz
+        //postgres://vkizatgz:NBribTWPJUXRh05uZE3WZkmDhHF-K4Um@balarama.db.elephantsql.com/vkizatgz
         {
-            NpgsqlConnection conn = new NpgsqlConnection("Server=Balarama.db.elephantsql.com; Username=vkizatgz;Password=NBribTWPJUXRh05uZE3WZkmDhHF-K4Um; Database=vkizatgz");
             conn.Open();
         }
-       
+
         public void InsertData(TempValue t, HumidValue h, PressureValue p, DateTime dt, string ipa)
         {
-           
+            var cmd = new NpgsqlCommand("INSERT INTO messwerte(temparatur, luftfeuchtigkeit, luftdruck) VALUES(@t,@h,@p)", conn);
+
+            cmd.Parameters.AddWithValue("t", @t.Value);
+            cmd.Parameters.AddWithValue("h", @h.Value);
+            cmd.Parameters.AddWithValue("p", @p.Value);
             
+            cmd.ExecuteNonQuery();
+
         }
+
+
     }
 }
